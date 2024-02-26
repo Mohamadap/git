cc3fb209111a6b40ab84ff182793f09513ad7eac
Mohamad <121693482+Mohamadap@users.noreply.github.com>
2024-02-26 02:37:03 -0800

    check user submit login

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const userNumber = 0;
 const age = 18;
+const addfun = function (user){
+  if(user==='mohamad'){
+    consol.log('welcome')
+  }else{
+    consol.log('invalid user')
+  }
+}
+addfun('mohamad');
